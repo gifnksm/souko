e3554012e61a03c80ce6dd03fa49238cabbdef0e
gifnksm <makoto.nksm+github@gmail.com>
2022-08-06 22:58:17 +0900

    Fix env-filter not working

--- src/bin/souko/main.rs
+++ src/bin/souko/main.rs
@@ -24,6 +24,7 @@ fn main() -> Result<()> {
     }
 
     tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .with_writer(std::io::stderr)
         .init();
     color_eyre::install()?;
