d1e3012b653033edc22c81327b21b8a160a1cd30
gifnksm <makoto.nksm+github@gmail.com>
2026-03-29 20:06:15 +0900

    Replace xtask dist with dedicated build-dist script

--- src/presentation/mod.rs
+++ src/presentation/mod.rs
@@ -1,6 +1,7 @@
-use std::env;
+use std::{env, io, process};
 
 use clap::{CommandFactory as _, Parser as _};
+use clap_complete::{Generator, Shell};
 pub use color_eyre::eyre::Result;
 
 use self::{args::Args, model::project_dirs::ProjectDirs};
@@ -16,7 +17,17 @@ pub(crate) struct Presentation {
 }
 
 impl Presentation {
-    pub(crate) fn from_env() -> Self {
+    pub(crate) fn from_env(bin_name: &str) -> Self {
+        let env_prefix = bin_name.to_uppercase().replace("-", "_");
+        if let Ok(shell) = env::var(format!("{env_prefix}_PRINT_COMPLETION")) {
+            Self::print_completion(bin_name, &shell);
+            process::exit(0);
+        }
+        if let Ok(output_dir) = env::var(format!("{env_prefix}_GENERATE_MAN_TO")) {
+            Self::generate_man(&output_dir);
+            process::exit(0);
+        }
+
         let args = Args::parse();
 
         if env::var_os("RUST_LOG").is_none() {
@@ -38,5 +49,27 @@ impl Presentation {
         ProjectDirs::init()?;
         self.args.run(service)?;
         Ok(())
+    }
+
+    fn print_completion(bin_name: &str, shell: &str) {
+        fn gen<G>(bin_name: &str, g: G)
+        where
+            G: Generator,
+        {
+            clap_complete::generate(g, &mut Presentation::command(), bin_name, &mut io::stdout());
+        }
+        match shell {
+            "bash" => gen(bin_name, Shell::Bash),
+            "elvish" => gen(bin_name, Shell::Elvish),
+            "fish" => gen(bin_name, Shell::Fish),
+            "powershell" => gen(bin_name, Shell::PowerShell),
+            "zsh" => gen(bin_name, Shell::Zsh),
+            "nushell" => gen(bin_name, clap_complete_nushell::Nushell),
+            _ => panic!("error: unknown shell `{shell}`, expected one of `bash`, `elvish`, `fish`, `powershell`, `zsh`, `nushell`"),
+        }
+    }
+
+    fn generate_man(output_dir: &str) {
+        clap_mangen::generate_to(Self::command(), output_dir).unwrap();
     }
 }
