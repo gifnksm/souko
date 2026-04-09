ef392a0554fec702b628e4402595f4ff443f9643
NAKASHIMA, Makoto <makoto.nksm+github@gmail.com>
2026-04-09 12:20:58 +0900

    refactor: ensure AppDirs stores absolute paths

--- src/app_dirs.rs
+++ src/app_dirs.rs
@@ -3,7 +3,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use color_eyre::eyre::{Result, eyre};
+use color_eyre::eyre::{Result, ensure, eyre};
 use directories::BaseDirs;
 
 #[derive(Debug, Clone)]
@@ -24,40 +24,58 @@ impl AppDirs {
         let project_dirs = directories::ProjectDirs::from("", "", app_name)
             .ok_or_else(|| eyre!("failed to get project directories"))?;
 
-        Ok(Self {
-            home_dir: base_dirs.home_dir().to_owned(),
-            config_dir: project_dirs.config_dir().to_owned(),
-            data_local_dir: project_dirs.data_local_dir().to_owned(),
-            cache_dir: project_dirs.cache_dir().to_owned(),
-        })
+        Self::from_dirs(
+            base_dirs.home_dir(),
+            project_dirs.config_dir(),
+            project_dirs.data_local_dir(),
+            project_dirs.cache_dir(),
+        )
     }
 
     pub(crate) fn new_for_test(app_name: &str, home_dir: impl Into<PathBuf>) -> Result<Self> {
         let home_dir = home_dir.into();
-        if cfg!(target_os = "windows") {
-            Ok(Self {
-                config_dir: home_dir.join(format!(r"AppData\Roaming\{app_name}\config")),
-                data_local_dir: home_dir.join(format!(r"AppData\Local\{app_name}\data")),
-                cache_dir: home_dir.join(format!(r"AppData\Local\{app_name}\cache")),
-                home_dir,
-            })
+        let (config_dir, data_local_dir, cache_dir) = if cfg!(target_os = "windows") {
+            (
+                home_dir.join(format!(r"AppData\Roaming\{app_name}\config")),
+                home_dir.join(format!(r"AppData\Local\{app_name}\data")),
+                home_dir.join(format!(r"AppData\Local\{app_name}\cache")),
+            )
         } else if cfg!(target_os = "linux") {
-            Ok(Self {
-                config_dir: home_dir.join(format!(".config/{app_name}")),
-                data_local_dir: home_dir.join(format!(".local/share/{app_name}")),
-                cache_dir: home_dir.join(format!(".cache/{app_name}")),
-                home_dir,
-            })
+            (
+                home_dir.join(format!(".config/{app_name}")),
+                home_dir.join(format!(".local/share/{app_name}")),
+                home_dir.join(format!(".cache/{app_name}")),
+            )
         } else if cfg!(target_os = "macos") {
-            Ok(Self {
-                config_dir: home_dir.join(format!("Library/Application Support/{app_name}")),
-                data_local_dir: home_dir.join(format!("Library/Application Support/{app_name}")),
-                cache_dir: home_dir.join(format!("Library/Caches/{app_name}")),
-                home_dir,
-            })
+            (
+                home_dir.join(format!("Library/Application Support/{app_name}")),
+                home_dir.join(format!("Library/Application Support/{app_name}")),
+                home_dir.join(format!("Library/Caches/{app_name}")),
+            )
         } else {
-            Err(eyre!("unsupported platform: {}", std::env::consts::OS))
-        }
+            return Err(eyre!("unsupported platform: {}", std::env::consts::OS));
+        };
+        Self::from_dirs(home_dir, config_dir, data_local_dir, cache_dir)
+    }
+
+    fn from_dirs<P, Q, R, S>(
+        home_dir: P,
+        config_dir: Q,
+        data_local_dir: R,
+        cache_dir: S,
+    ) -> Result<Self>
+    where
+        P: AsRef<Path> + Into<PathBuf>,
+        Q: AsRef<Path> + Into<PathBuf>,
+        R: AsRef<Path> + Into<PathBuf>,
+        S: AsRef<Path> + Into<PathBuf>,
+    {
+        Ok(Self {
+            home_dir: ensure_absolute_path("home_dir", home_dir)?.into(),
+            config_dir: ensure_absolute_path("config_dir", config_dir)?.into(),
+            data_local_dir: ensure_absolute_path("data_local_dir", data_local_dir)?.into(),
+            cache_dir: ensure_absolute_path("cache_dir", cache_dir)?.into(),
+        })
     }
 
     pub(crate) fn home_dir(&self) -> &Path {
@@ -75,4 +93,16 @@ impl AppDirs {
     pub(crate) fn cache_dir(&self) -> &Path {
         &self.cache_dir
     }
+}
+
+fn ensure_absolute_path<P>(name: &str, path: P) -> Result<P>
+where
+    P: AsRef<Path>,
+{
+    ensure!(
+        path.as_ref().is_absolute(),
+        "{name} is not an absolute path: {}",
+        path.as_ref().display()
+    );
+    Ok(path)
 }
